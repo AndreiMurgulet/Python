061a13f3a1c60ce7007f4f131831a42079d1cbdd
AndreiMurgulet <126000471+AndreiMurgulet@users.noreply.github.com>
2025-01-05 00:20:36 +0200

    Add files via upload
    Interfata de start

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Module Name: main.py
+Purpose: this module represents the entrance point of the game
+"""
+import sys
+import tkinter as tk
+
+from ui_welcome import MancalaWelcome
+
+
+def main():
+    if len(sys.argv) > 1:
+        opponent_type = sys.argv[1]
+    else:
+        opponent_type = "human"
+
+    root = tk.Tk()
+    root.title("Mancala - Main")
+
+    welcome_screen = MancalaWelcome(root, opponent_type)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ ui_welcome.py
@@ -0,0 +1,41 @@
+"""
+Module Name: ui_welcome.py
+Purpose: this module implements the interface for the welcome screen
+"""
+import tkinter as tk
+from PIL import Image, ImageTk
+from PIL.Image import Resampling
+
+
+class MancalaWelcome:
+    """
+    Class to initialize the welcome interface and start button
+    """
+
+    def __init__(self, root, opponent_type="human"):
+        """Initialize the game state and start button  """
+        self.root = root
+        self.root.title("Mancala - Ecran Bun Venit")
+        self.opponent_type = opponent_type
+
+        self.canvas_width = 900
+        self.canvas_height = 400
+
+        self.canvas = tk.Canvas(root, width=self.canvas_width, height=self.canvas_height)
+        self.canvas.pack(fill="both", expand=True)
+
+        self.bg_image = Image.open("Mancala_background.jpg")
+        self.bg_image = self.bg_image.resize((self.canvas_width, self.canvas_height), Resampling.LANCZOS)
+        self.bg_image_tk = ImageTk.PhotoImage(self.bg_image)
+
+        self.canvas.create_image(0, 0, image=self.bg_image_tk, anchor='nw')
+
+        self.label = tk.Label(root, text="Welcome to the Game of Mancala!", font=("Calibri", 16))
+        self.start_button = tk.Button(root, text="Start Game", font=("Calibri", 14), command=self.start_game)
+
+        self.canvas.create_window(self.canvas_width / 2, 50, anchor="n", window=self.label)
+        self.canvas.create_window(self.canvas_width / 2, 160, anchor="n", window=self.start_button)
+
+    def start_game(self):
+        """Begin the game"""
+        self.canvas.destroy()
